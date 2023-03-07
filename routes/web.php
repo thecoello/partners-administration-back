e86e987216369085f25047a3c365b5a4121acf08
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2023-03-07 17:54:37 +0100

    Implementación de rutas nuevas para API

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,25 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->delete('deleteuser/{id}', ['uses' => 'UsersController@deteleUser']);
     $router->put('putuser/{id}', ['uses' => 'UsersController@updateUser']);
 
-  });
+    //PACKGES
+    $router->get('getpackages',  ['uses' => 'PackagesController@showAllPackages']);
+    $router->get('getpackage',  ['uses' => 'PackagesController@showPackage']);
+    $router->get('postpackage',  ['uses' => 'PackagesController@createPackage']);
+    $router->get('deletepackage',  ['uses' => 'PackagesController@detelePackage']);
+    $router->get('putpackage',  ['uses' => 'PackagesController@updatePackage']);
+
+    //LOCATIONS
+    $router->get('getlocations',  ['uses' => 'LocationsController@showAllLocations']);
+    $router->get('getlocation',  ['uses' => 'LocationsController@showLocation']);
+    $router->get('postlocation',  ['uses' => 'LocationsController@createLocation']);
+    $router->get('deletelocation',  ['uses' => 'LocationsController@deteleLocation']);
+    $router->get('putlocation',  ['uses' => 'LocationsController@updateLocation']);
+
+    //INVOICES
+    $router->get('getinvoices',  ['uses' => 'InvoicesController@showAllInvoices']);
+    $router->get('getinvoice',  ['uses' => 'InvoicesController@showInvoice']);
+    $router->get('postinvoice',  ['uses' => 'InvoicesController@createInvoice']);
+    $router->get('deleteinvoice',  ['uses' => 'InvoicesController@deteleInvoice']);
+    $router->get('putinvoice',  ['uses' => 'InvoicesController@updateInvoice']);
+
+});
