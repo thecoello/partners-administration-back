e4cc9127fff949f84d9bd23c06e6165af37e0b7a
Manuel Guerra Coello <thecoello@gmail.com>
2023-11-14 14:23:16 +0100

    *

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -14,7 +14,7 @@ class InvoiceController extends Controller
         $event = DB::table('eventinfos')->get();
 
         $invoices =  DB::table('invoices')->where('invoices.invoice_number','!=','null')->leftJoin('packages','invoices.category','=','packages.id')->leftJoin('users','invoices.user_id','=','users.id')->leftJoin('locations','invoices.location','=','locations.id')->select(
-        "invoices.id","pricetype","company_name","category","location","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file")->orderBy('invoices.invoice_number','asc')->simplePaginate(15);
+            "invoices.id","invoices.user_id","pricetype","company_name","category","location","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file","coupons")->orderBy('invoices.invoice_number','asc')->simplePaginate(15);
 
         if($event && $invoices){
             return response()->json(['eventinfo' => $event, 'invoices' => $invoices]);
@@ -28,8 +28,8 @@ class InvoiceController extends Controller
     {
         $event = DB::table('eventinfos')->get();
 
-        $invoices =  DB::table('invoices')->where('invoices.id','=',$id)->leftJoin('packages','invoices.category','=','packages.id')->leftJoin('users','invoices.user_id','=','users.id')->leftJoin('locations','invoices.location','=','locations.id')->select(
-        "pricetype","company_name","category","location","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file")->orderBy('invoices.invoice_number','asc')->simplePaginate(15);
+        $invoices =  DB::table('invoices')->where('invoices.id','=',$id)->leftJoin('packages','invoices.category','=','packages.id')->leftJoin('users','invoices.user_id','=','users.id')->leftJoin('locations','invoices.location','=','locations.id')->select("invoices.id","invoices.user_id",
+        "pricetype","company_name","category","location","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file","coupons")->orderBy('invoices.invoice_number','asc')->simplePaginate(15);
 
         if($event && $invoices){
             return response()->json(['eventinfo' => $event, 'invoices' => $invoices]);
@@ -44,8 +44,8 @@ class InvoiceController extends Controller
 
         $event = DB::table('eventinfos')->get();
 
-        $invoices =  DB::table('invoices')->where('invoices.invoice_number','!=','null')->leftJoin('packages','invoices.category','=','packages.id')->leftJoin('users','invoices.user_id','=','users.id')->leftJoin('locations','invoices.location','=','locations.id')->orderBy('invoices.invoice_number','asc')->where('invoices.company_name','like','%'.$search.'%')->orWhere('invoices.invoice_number','like','%'.$search.'%')->select(
-        "pricetype","company_name","pack_name","location_name","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file")->simplePaginate(15);
+        $invoices =  DB::table('invoices')->where('invoices.invoice_number','!=','null')->leftJoin('packages','invoices.category','=','packages.id')->leftJoin('users','invoices.user_id','=','users.id')->leftJoin('locations','invoices.location','=','locations.id')->orderBy('invoices.invoice_number','asc')->where('invoices.company_name','like','%'.$search.'%')->orWhere('invoices.invoice_number','like','%'.$search.'%')->select("invoices.id","invoices.user_id",
+        "pricetype","company_name","pack_name","location_name","quantity","vat","subtotal","iva","total","address","zip","country","invoice_number","payment_status","payment_method","invoice_date","name","contact","email","contract_file","coupons")->simplePaginate(15);
 
         if($event && $invoices){
             return response()->json(['eventinfo' => $event, 'invoices' => $invoices]);
@@ -89,13 +89,15 @@ class InvoiceController extends Controller
             "subtotal" => $request->subtotal,
             "iva" => $iva,
             "total" => $total,       
-            "invoice_number" => $eventInfo[0]->invoice_pre . $eventInfo[0]->invoice_number,
+            "invoice_number" => $eventInfo[0]->invoice_pre . str_pad( $eventInfo[0]->invoice_number,3,'0',STR_PAD_LEFT),
             "contract_file" => 'public/contracts/' . time() . "_" . $request->name . '_' . 'contract' . '.pdf',
             "company_name" => $request->company_name,
             "address" => $request->address,
             "zip" => $request->zip,
             "country" => $request->country,
-            "vat" => $request->vat
+            "vat" => $request->vat,
+            "payment_method" => $request->payment_method,
+            "payment_status" => $request->payment_status
         ];
 
         DB::table('eventinfos')->update(array('invoice_number'=>($eventInfo[0]->invoice_number + 1)));
@@ -106,8 +108,60 @@ class InvoiceController extends Controller
 
     public function putInvoices($id, Request $request)
     {
-        $result = DB::table('invoices')->where('id', $id)->update(request()->all());
-        return $result;
+        $iva = 0;
+        $total = 0;
+        $invoiceRequest = [];
+
+        $eventInfo = DB::table('eventinfos')->get();
+
+        if($request->country === "Spain"){
+            $iva = (((float)$request->subtotal * (int)$eventInfo[0]->iva)/100);
+            $total = (float)$request->subtotal + $iva;
+        }else{
+            $total = (float)$request->subtotal;  
+        }
+        
+        if( $request->file('contract_file')){
+            $request->file('contract_file')->move('public/contracts/', time() . "_" . $request->name . '_' . 'contract' . '.pdf');
+            $invoiceRequest = [
+                "user_id" => $request->user_id,
+                "category" => $request->category,
+                "location" => $request->location,
+                "pricetype" => $request->pricetype,
+                "subtotal" => $request->subtotal,
+                "iva" => $iva,
+                "total" => $total,       
+                "company_name" => $request->company_name,
+                "address" => $request->address,
+                "zip" => $request->zip,
+                "country" => $request->country,
+                "vat" => $request->vat,
+                "contract_file" => 'public/contracts/' . time() . "_" . $request->name . '_' . 'contract' . '.pdf',
+                "coupons" => $request->coupons,
+                "payment_method" => $request->payment_method,
+                "payment_status" => $request->payment_status
+
+            ];
+        }else{
+            $invoiceRequest = [
+                "user_id" => $request->user_id,
+                "category" => $request->category,
+                "location" => $request->location,
+                "pricetype" => $request->pricetype,
+                "subtotal" => $request->subtotal,
+                "iva" => $iva,
+                "total" => $total,       
+                "company_name" => $request->company_name,
+                "address" => $request->address,
+                "zip" => $request->zip,
+                "country" => $request->country,
+                "vat" => $request->vat,
+                "coupons" => $request->coupons,
+                "payment_method" => $request->payment_method,
+                "payment_status" => $request->payment_status                
+            ];
+        }
+        return DB::table('invoices')->where('id', $id)->update($invoiceRequest);
     }
 
     public function putInvoicesUser($id, Request $request)
